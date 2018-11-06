30691638d74a7ad1e0ce02e9956dce8f92315ad1
niebloomj <niebloomj@gmail.com>
2018-11-06 16:20:57 -0500

    switched libraries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,125 +1,214 @@
-import {AfterViewChecked, AfterViewInit, Component, OnInit} from '@angular/core';
-import 'fullcalendar';
-import * as $ from 'jquery';
-// @ts-ignore
-import {Options} from 'fullcalendar';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component} from '@angular/core';
+import {CalendarEvent, CalendarWeekViewBeforeRenderEvent} from 'angular-calendar';
+import {ViewPeriod} from 'calendar-utils';
+import moment from 'moment-timezone';
+import {RRule} from 'rrule';
+
+interface RecurringEvent {
+  title: string;
+  color: any;
+  start: number;
+  end: number;
+  rrule?: {
+    freq: any;
+    bymonth?: number;
+    bymonthday?: number;
+    byweekday?: any;
+  };
+}
+
+moment.tz.setDefault('est');
+const colors: any = {
+  red: {
+    primary: '#ad2121',
+    secondary: '#FAE3E3'
+  },
+  blue: {
+    primary: '#3764cf',
+    secondary: '#ffffff'
+  },
+  yellow: {
+    primary: '#e3bc08',
+    secondary: '#FDF1BA'
+  }
+};
 
 @Component({
   selector: 'app-root',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['app.component.css']
 })
-export class AppComponent implements AfterViewChecked, AfterViewInit, OnInit {
-
-  calendarOptions: Options;
-  events: Array<any> = [
+export class AppComponent {
+  viewDate: Date = new Date();
+  recurringEvents: RecurringEvent[] = [
     {
-      'events': [
-        {
-          title: 'Gemara, R. Lisbon',
-          start: '5:50:00',
-          end: '6:30:00',
-          dow: [1, 2, 3, 4, 5]
-        },
-        {
-          title: 'Gemara, R. Lisbon',
-          start: '6:50',
-          end: '7:30',
-          dow: [0]
-        },
-        {
-          title: 'Gemara, R. Shaffer',
-          start: '18:00',
-          end: '19:00',
-          dow: [0]
-        },
-        {
-          title: 'Gemara, R. Shaffer',
-          start: '19:00',
-          end: '20:00',
-          dow: [2]
-        },
-        {
-          title: 'Samech Vav, R. Avraham Levitan',
-          start: '20:30',
-          end: '21:30',
-          dow: [0]
-        },
-        {
-          title: 'Understanding Tefillah - Slavatiscki',
-          start: '21:15',
-          end: '22:15',
-          dow: [1]
-        },
-        {
-          title: 'Shir and Beer, R. Raices',
-          start: '21:15',
-          end: '22:30',
-          dow: [3]
-        },
-        {
-          title: 'Kolel Erev, YOUng Anash',
-          start: '21:00',
-          end: '22:00',
-          dow: [4]
-        },
-        {
-          title: 'Ayin Beis, R. Kaplan',
-          start: '21:00',
-          end: '22:00',
-          dow: [6]
-        },
-      ]
+      title: 'Gemara, R. Lisbon',
+      color: colors.blue,
+      start: 60 * 5 + 50,
+      end: 60 * 6 + 30,
+      rrule: {
+        freq: RRule.WEEKLY,
+        byweekday: [RRule.MO, RRule.TU, RRule.WE, RRule.TH, RRule.FR]
+      },
     },
     {
-      'events': [
-        {
-          title: 'Lady’s Chassidus, R. Lisbon',
-          start: '13:00',
-          end: '14:00',
-          dow: [1]
-        },
-        {
-          title: 'Lady’s Tanya, R. Lisbon',
-          start: '19:30',
-          end: '20:30',
-          dow: [3]
-        }
-      ],
-      'color': 'pink',
-      'textColor': 'green'
-    }
+      title: 'Gemara, R. Lisbon',
+      color: colors.blue,
+      start: 60 * 6 + 50,
+      end: 60 * 7 + 30,
+      rrule: {
+        freq: RRule.WEEKLY,
+        byweekday: [RRule.SU]
+      },
+    },
+    {
+      title: 'Gemara, R. Shaffer',
+      color: colors.blue,
+      start: 60 * 18 + 0,
+      end: 60 * 19 + 0,
+      rrule: {
+        freq: RRule.WEEKLY,
+        byweekday: [RRule.SU]
+      },
+    },
+    {
+      title: 'Gemara, R. Shaffer',
+      color: colors.blue,
+      start: 60 * 19,
+      end: 60 * 20,
+      rrule: {
+        freq: RRule.WEEKLY,
+        byweekday: [RRule.TU]
+      },
+    },
+    {
+      title: 'Samech Vav, R. Avraham Levitan',
+      color: colors.blue,
+      start: 60 * 20 + 30,
+      end: 60 * 21 + 30,
+      rrule: {
+        freq: RRule.WEEKLY,
+        byweekday: [RRule.SU]
+      },
+    },
+    {
+      title: 'Understanding Tefillah, Slavatiscki',
+      color: colors.blue,
+      start: 60 * 21 + 15,
+      end: 60 * 22 + 15,
+      rrule: {
+        freq: RRule.WEEKLY,
+        byweekday: [RRule.MO]
+      },
+    },
+    {
+      title: 'Shir and Beer, R. Raices',
+      color: colors.blue,
+      start: 60 * 21 + 15,
+      end: 60 * 22 + 30,
+      rrule: {
+        freq: RRule.WEEKLY,
+        byweekday: [RRule.WE]
+      },
+    },
+    {
+      title: 'Kolel Erev, YOUng Anash',
+      color: colors.blue,
+      start: 60 * 21,
+      end: 60 * 22,
+      rrule: {
+        freq: RRule.WEEKLY,
+        byweekday: [RRule.TH]
+      },
+    },
+    {
+      title: 'Ayin Beis, R. Kaplan',
+      color: colors.blue,
+      start: 60 * 21,
+      end: 60 * 22,
+      rrule: {
+        freq: RRule.WEEKLY,
+        byweekday: [RRule.SA]
+      },
+    },
+    {
+      title: 'Lady’s Chassidus, R. Lisbon',
+      color: colors.red,
+      start: 60 * 13,
+      end: 60 * 14,
+      rrule: {
+        freq: RRule.WEEKLY,
+        byweekday: [RRule.MO]
+      },
+    },
+    {
+      title: 'Lady’s Tanya, R. Lisbon',
+      color: colors.red,
+      start: 60 * 19 + 30,
+      end: 60 * 20 + 30,
+      rrule: {
+        freq: RRule.WEEKLY,
+        byweekday: [RRule.WE]
+      },
+    },
+    {
+      title: 'Chassidus, R. Lisbon',
+      color: colors.blue,
+      start: 60 * 8 + 45,
+      end: 60 * 9 + 30,
+      rrule: {
+        freq: RRule.WEEKLY,
+        byweekday: [RRule.SA]
+      },
+    },
   ];
 
-  ngAfterViewChecked(): void {
-    $('#calendar').fullCalendar('rerenderEvents');
+  calendarEvents: CalendarEvent[] = [];
+
+  viewPeriod: ViewPeriod;
+
+  constructor(private cdr: ChangeDetectorRef) {
   }
 
-  ngAfterViewInit(): void {
-    setTimeout(() => {
-      $('#calendar').fullCalendar('rerenderEvents');
-    }, 100);
+  updateCalendarEvents(
+    viewRender:
+      | CalendarWeekViewBeforeRenderEvent
+  ): void {
+    if (
+      !this.viewPeriod ||
+      !moment(this.viewPeriod.start).isSame(viewRender.period.start) ||
+      !moment(this.viewPeriod.end).isSame(viewRender.period.end)
+    ) {
+      this.viewPeriod = viewRender.period;
+      this.calendarEvents = [];
+
+      this.recurringEvents.forEach(event => {
+        const rule: RRule = new RRule({
+          ...event.rrule,
+          dtstart: moment(viewRender.period.start)
+            .startOf('day')
+            .toDate(),
+          until: moment(viewRender.period.end)
+            .endOf('day')
+            .toDate()
+        });
+        const {title, color} = event;
+        rule.all().forEach(date => {
+          console.log(moment(moment(date).add(event.start, 'minutes').toDate()).format('H:mm'));
+          this.calendarEvents.push({
+            title,
+            color,
+            start: moment(date).add(event.start, 'minutes').toDate(),
+            end: moment(date).add(event.end, 'minutes').toDate()
+          });
+        });
+      });
+      this.cdr.detectChanges();
+    }
   }
 
-  ngOnInit(): void {
-    // @ts-ignore
-    this.calendarOptions = {
-      editable: false,
-      eventLimit: false,
-      defaultView: 'agendaWeek',
-      minTime: '05:00:00',
-      maxTime: '23:00:00',
-      allDaySlot: false,
-      header: false,
-      slotDuration: '0:30',
-      slotLabelInterval: '1:00',
-      height: 650,
-      ratio: 2,
-      // agendaEventMinHeight: 70,
-      nowIndicator: true,
-      eventSources: this.events
-    };
-
-    $('#calendar').fullCalendar(this.calendarOptions);
+  moment(test) {
+    return moment(test);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,14 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {CalendarDateFormatter, CalendarModule, CalendarMomentDateFormatter, DateAdapter, MOMENT} from 'angular-calendar';
+import moment from 'moment-timezone';
+import {adapterFactory} from 'angular-calendar/date-adapters/moment';
+import {CommonModule} from '@angular/common';
+
+export function momentAdapterFactory() {
+  return adapterFactory(moment);
+}
 
 @NgModule({
   declarations: [
@@ -9,9 +17,27 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     NgbModule,
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    CalendarModule.forRoot(
+      {
+        provide: DateAdapter,
+        useFactory: momentAdapterFactory
+      },
+      {
+        dateFormatter: {
+          provide: CalendarDateFormatter,
+          useClass: CalendarMomentDateFormatter
+        }
+      }
+    )
   ],
-  providers: [],
+  providers: [
+    {
+      provide: MOMENT,
+      useValue: moment
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
